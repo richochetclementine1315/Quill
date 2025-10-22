4a5d29406ef295ca910dbfe983fa2865dee03cab
Mrinmoy_1315 <mrinmoymatilal1315@gmail.com>
2025-10-23 05:07:21 +0530

    CORS issues

--- QuillBackend/main.go
+++ QuillBackend/main.go
@@ -20,7 +20,7 @@ func main() {
 
 	// CORS middleware
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000",
+		AllowOrigins:     "https://quill-wpb7.onrender.com",
 		AllowCredentials: true,
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
